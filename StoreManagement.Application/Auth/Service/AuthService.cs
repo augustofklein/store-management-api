3ea35e72a065b8de3fab8ea455967fc907157d51
Augusto Fernando Klein <augusto_klein@hotmail.com>
2024-12-21 21:37:58 -0300

    Add dependency injection

--- StoreManagement.Application/Auth/Service/AuthService.cs
+++ StoreManagement.Application/Auth/Service/AuthService.cs
@@ -1,10 +1,11 @@
 using CSharpFunctionalExtensions;
+using StoreManagement.Infrastructure.DBContext;
 
 namespace StoreManagement.Application.Auth.Service
 {
-    public class AuthService : IAuthService
+    public class AuthService(AppDbContext dbContext) : IAuthService
     {
-        //private readonly AppDbContext _dbContext;
+        private readonly AppDbContext _dbContext = dbContext;
         public async Task<Result> ValidateLogin(string username, string password, CancellationToken cancellationToken)
         {
             return Result.Success();
