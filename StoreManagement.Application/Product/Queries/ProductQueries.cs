aca79a7edad160184c82bb466edf218fc586c1fd
Augusto Klein <augusto_klein@hotmail.com>
2025-11-16 20:23:59 -0300

    Add CompanyEntity class.

--- StoreManagement.Application/Product/Queries/ProductQueries.cs
+++ StoreManagement.Application/Product/Queries/ProductQueries.cs
@@ -7,27 +7,19 @@ namespace StoreManagement.Application.Product.Queries
 {
     public class ProductQueries(AppDbContext dbContext) : IProductQueries
     {
-        private readonly AppDbContext _dbContext = dbContext;
         public async Task<Result<IEnumerable<ProductDto>>> GetProducts(CancellationToken cancellationToken)
         {
-            try
-            {
-                var result = await _dbContext.Products.AsNoTracking().ToListAsync(cancellationToken);
+            var result = await dbContext.Products.AsNoTracking().ToListAsync(cancellationToken);
 
-                var products = result.Select(r => new ProductDto
-                {
-                    Id = r.Id,
-                    Barcode = r.Barcode,
-                    Description = r.Description,
-                    Stock = r.Stock
-                });
-
-                return Result.Success(products);
-            }
-            catch(Exception e)
+            var products = result.Select(r => new ProductDto
             {
-                return Result.Failure<IEnumerable<ProductDto>>($"An error occurred: {e.Message}");
-            }
+                Id = r.Id,
+                Barcode = r.Barcode,
+                Description = r.Description,
+                Stock = r.Stock
+            });
+
+            return Result.Success(products);
         }
     }
 }
---
+++ StoreManagement.Infrastructure/DBContext/Model/CompanyEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreManagement.Infrastructure.DBContext.Model
+{
+    public class CompanyEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public CompanyEntity(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
