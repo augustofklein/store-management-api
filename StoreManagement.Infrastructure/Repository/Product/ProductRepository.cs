c8b386ff6ddaf8e8fd9b0dca2afab4f1280ea9f7
Augusto Klein <augusto_klein@hotmail.com>
2025-11-16 20:15:31 -0300

    Get out try catch in product repository.

--- StoreManagement.Infrastructure/Repository/Product/ProductRepository.cs
+++ StoreManagement.Infrastructure/Repository/Product/ProductRepository.cs
@@ -6,75 +6,50 @@ namespace StoreManagement.Infrastructure.Repository.Product
 {
     public class ProductRepository(AppDbContext dbContext) : IProductRepository
     {
-        private readonly AppDbContext _dbContext = dbContext;
-
         public async Task<Result> AddProduct(int id, string barcode, string description, int stock, CancellationToken cancellationToken)
         {
-            try
+            var product = new ProductEntity
             {
-                var product = new ProductEntity
-                {
-                    Id = id,
-                    Barcode = barcode,
-                    Description = description,
-                    Stock = stock
-                };
+                Id = id,
+                Barcode = barcode,
+                Description = description,
+                Stock = stock
+            };
 
-                await _dbContext.Products.AddAsync(product, cancellationToken);
-                await _dbContext.SaveChangesAsync(cancellationToken);
+            await dbContext.Products.AddAsync(product, cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
 
-                return Result.Success();
-            }
-            catch(Exception ex)
-            {
-                return Result.Failure($"An error occurred while adding the product: {ex.Message}");
-            }
+            return Result.Success();
         }
 
         public async Task<Result> RemoveProduct(int id, CancellationToken cancellationToken)
         {
-            try
+            var product = await dbContext.Products.FindAsync([id], cancellationToken);
+            
+            if (product == null)
             {
-                var product = await _dbContext.Products.FindAsync([id], cancellationToken);
-                
-                if (product == null)
-                {
-                    return Result.Failure($"Product with ID {id} not found.");
-                }
-
-                _dbContext.Products.Remove(product);
-
-                await _dbContext.SaveChangesAsync(cancellationToken);
-
-                return Result.Success();
+                return Result.Failure($"Product with ID {id} not found.");
             }
-            catch(Exception ex)
-            {
-                return Result.Failure($"An error occurred while removing the product: {ex.Message}");
-            }
+
+            dbContext.Products.Remove(product);
+
+            return Result.Success();
         }
 
         public async Task<Result> EditProduct(int id, string description, CancellationToken cancellationToken)
         {
-            try
+            var product = await dbContext.Products.FindAsync([id], cancellationToken);
+            
+            if (product == null)
             {
-                var product = await _dbContext.Products.FindAsync([id], cancellationToken);
-                
-                if (product == null)
-                {
-                    return Result.Failure($"Product with ID {id} not found.");
-                }
-
-                product.Description = description;
-
-                await _dbContext.SaveChangesAsync(cancellationToken);
-
-                return Result.Success();
+                return Result.Failure($"Product with ID {id} not found.");
             }
-            catch(Exception ex)
-            {
-                return Result.Failure($"An error occurred while updating the product: {ex.Message}");
-            }
+
+            product.Description = description;
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
         }
     }
 }
